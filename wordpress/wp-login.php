752cf1aa2f2b213443df3ec9e7869e874d884eec
fanzhuguo <fanzhuguo123@qq.com>
2015-10-22 15:47:40 +0800

    git test

--- wordpress/wp-login.php
+++ wordpress/wp-login.php
@@ -10,7 +10,7 @@
 
 /** Make sure that the WordPress bootstrap has run before continuing. */
 require( dirname(__FILE__) . '/wp-load.php' );
-
+//20151022
 // Redirect to https login if forced to use SSL
 if ( force_ssl_admin() && ! is_ssl() ) {
 	if ( 0 === strpos($_SERVER['REQUEST_URI'], 'http') ) {
